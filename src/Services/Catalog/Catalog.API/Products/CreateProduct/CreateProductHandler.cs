0b223246c582748290f0c5e7814028c30c815f02
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-03-19 19:15:12 -0500

    Add Product feature and basic handler

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, 
+    List<string> Category, 
+    string Description, 
+    string ImageFile, 
+    decimal Price) : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id) : IRequest<CreateProductResult>;
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Business logic
+        throw new NotImplementedException();
+    }
+}
