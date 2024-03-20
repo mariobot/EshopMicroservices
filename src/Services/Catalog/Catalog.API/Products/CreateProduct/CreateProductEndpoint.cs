ac35e31b05d9affdff883c83e5b64c6e58d3b103
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-03-20 17:32:17 -0500

    Include Mapster and Carter libraries to BuildingBlocks

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,15 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using Carter;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(string Name,  List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
 {
-    public class CreateProductEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        
     }
 }
