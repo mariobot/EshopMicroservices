9a45f855b25ca1e89bbd81656b1ac9486eda811e
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-03-20 18:53:20 -0500

    Update service to write on database

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -7,6 +7,10 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(config =>
+{
+    config.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
