7d15d7f4bd4d6a70c77a622f4a2b35115a132e78
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-03-22 16:33:20 -0500

    Add GetProductById implementation

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdRequest();
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{Id}", async (Guid Id, ISender sender) => 
+        {
+            var request = await sender.Send(new GetProductByIdQuery(Id));
+
+            var result = request.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(result);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Product by Id")
+        .WithDescription("Get Product by Id");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
+
+public class GetProductByIdQueryHandler(IDocumentSession documentSession, ILogger<GetProductByIdQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"GetProductByIdQueryHandler called with the query {query}", query);
+
+        var product = await documentSession.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+    }
+}
