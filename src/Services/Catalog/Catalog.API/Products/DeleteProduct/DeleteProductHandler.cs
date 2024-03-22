945b34370c729afb5fccf4cd22963e7ef17d274d
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-03-22 17:32:18 -0500

    Include DeleteProduct Implementation.

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id): ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHandler(IDocumentSession documentSession, ILogger<DeleteProductCommandHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductCommandHanlder called with the command {@command}", command);
+
+        var product = await documentSession.LoadAsync<Product>(command.Id);
+
+        if (product == null)         
+        {
+            throw new ProductNotFoundException();
+        }
+
+        documentSession.Delete<Product>(command.Id);
+        await documentSession.SaveChangesAsync();
+
+        return new DeleteProductResult(true);
+    }
+}
