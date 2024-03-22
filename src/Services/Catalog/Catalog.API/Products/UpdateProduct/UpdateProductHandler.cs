550732cd121557367531d1e32af9195a8daa1927
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-03-22 17:32:05 -0500

    Include UpdateProduct implementation

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<UpdateProductResult>;
+
+public record UpdateProductResult(bool IsSuccess);
+
+internal class UpdateProductCommandHandler(IDocumentSession documentSession, ILogger<UpdateProductCommandHandler> logger) 
+    : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("UpdateProductCommandHandler called by the command");
+
+        var product = await documentSession.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (product == null) 
+        {
+            throw new ProductNotFoundException();
+        }
+
+        product.Name = command.Name;
+        product.Category = command.Category;
+        product.Description = command.Description;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+
+        documentSession.Update(product);
+
+        await documentSession.SaveChangesAsync(cancellationToken);
+
+        return new UpdateProductResult(true);
+    }
+}
