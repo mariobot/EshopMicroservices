76a324d698bd4ad76386464af1bbbf63d5407f6c
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-04-19 17:11:29 -0500

    Create Shopping clasess

---
+++ src/Services/Basket/Basket.API/Models/ShopingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Models;
+
+public class ShopingCart
+{
+    public string Username { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public ShopingCart(string username)
+    {
+        this.Username = username;
+    }
+
+    public ShopingCart()
+    {
+        
+    }
+}
