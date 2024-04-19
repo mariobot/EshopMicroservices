cf6d06eeca3f5309971937744aa3e314dd0f7c1d
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-04-19 17:23:10 -0500

    Initialize basic user cases for Basket

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShopingCart Cart);
+
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        return new GetBasketResult(new ShopingCart("tmp"));
+    }
+}
