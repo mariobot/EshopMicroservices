c71b78c9bbfdf4483ad35acc60c634866234aeaa
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-04-19 18:34:52 -0500

    Include Exception basket not found

---
+++ src/Services/Basket/Basket.API/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.API.Exceptions;
+
+public class BasketNotFoundException : NotFoundException
+{
+    public BasketNotFoundException(string userName) : base("Basket", userName)
+    {
+    }
+}
