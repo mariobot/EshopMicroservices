49746af0f1f8fdfc76a8811ecae8cb73a338f4b6
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-04-19 18:02:58 -0500

    Register Carte, Mediatr and validation and loggin bh to the project.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline
 
+app.MapCarter();
 app.Run();
