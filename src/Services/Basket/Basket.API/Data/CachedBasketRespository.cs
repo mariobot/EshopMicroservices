268dc13db3b8a67092cafd33cfc11269deda0557
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-04-26 18:15:14 -0500

    Create CachedBasketRepository.cs

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRespository.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Basket.API.Data;
+
+public class CachedBasketRespository(IBasketRepository repository) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        return await repository.GetBasket(userName, cancellationToken);
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        return await repository.StoreBasket(basket, cancellationToken);
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        return await repository.DeleteBasket(userName, cancellationToken);
+    }
+}
