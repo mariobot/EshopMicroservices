2c840260824833e13525630d46816c37eba4943d
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-04-26 18:05:24 -0500

    Add ExceptionHandler to basket api

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using BuildingBlocks.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,10 +22,15 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 
 app.MapCarter();
+app.UseExceptionHandler(options =>
+{
+
+});
 app.Run();
