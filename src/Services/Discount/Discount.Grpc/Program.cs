2d8b346edc8f5548fad8d55f184095202b2358c0
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-04-29 16:38:48 -0500

    Delete unused geet.proto

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,3 @@
-using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +7,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
