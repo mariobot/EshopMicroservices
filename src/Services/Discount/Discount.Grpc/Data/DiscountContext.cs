e0aba6a8bd6ac9e6305c4b5d6ee249a5ebc8b0bf
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-04-29 17:18:35 -0500

    Add Initial script and auto migration command

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -8,7 +8,14 @@ public class DiscountContext : DbContext
     public DbSet<Coupon> Coupons { get; set; } = default!;
 
     public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+    {        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150},
+            new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+            );
     }
 }
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<DiscountContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMigration();
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with Discount.gRPC endpoints");
 
