db265d8011bd7e771f29eb2c96be7bb56be67b53
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-01 18:12:04 -0500

    Add ApplicationDbContext class and nuget references

--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -3,8 +3,8 @@
 public record CustomerId
 {
     public Guid Value { get; }
-    private CustomerId(Guid value) => Value = value;
-    private static CustomerId Of(Guid value)
+    public CustomerId(Guid value) => Value = value;
+    public static CustomerId Of(Guid value)
     {
         ArgumentNullException.ThrowIfNull(value);
         if (value == Guid.Empty)
---
+++ src/Services/Ordering/Ordering.Infraestructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System.Reflection;
+
+namespace Ordering.Infraestructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {        
+    }
+
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        // This line of code configure the configuration files automatic from the assembly
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
