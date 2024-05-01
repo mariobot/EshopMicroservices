628556e15d31d325994a4c01df2986a6155aea34
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-04-30 19:34:01 -0500

    Add the Abstractions clases and interfaces.

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+{
+    private readonly List<IDomainEvent> domainEvents = new();
+    public IReadOnlyList<IDomainEvent> DomainEvents => domainEvents.AsReadOnly();
+
+    public void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        this.domainEvents.Add(domainEvent);
+    }
+
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        IDomainEvent[] dequeueEvents = this.domainEvents.ToArray();
+
+        this.domainEvents.Clear();
+
+        return dequeueEvents;
+    }
+}
