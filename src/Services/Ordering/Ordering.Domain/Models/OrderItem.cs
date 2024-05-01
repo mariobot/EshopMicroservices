40c885a0e89bff1eeb9cffa15443d0906d9ac79a
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-01 15:59:50 -0500

    Define the propertyId for each Model

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -1,9 +1,10 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class OrderItem : Entity<Guid>
+public class OrderItem : Entity<OrderItemId>
 {
-    internal OrderItem(Guid orderId, Guid productId, int quantity, int price)
+    internal OrderItem(OrderId orderId, ProductId productId, int quantity, int price)
     {
+        Id = OrderItemId.Of(Guid.NewGuid());
         OrderId = orderId;
         ProductId = productId;
         Quantity = quantity;
@@ -11,8 +12,8 @@ public class OrderItem : Entity<Guid>
         
     }
 
-    public Guid OrderId { get; private set; } = default!;
-    public Guid ProductId { get; private set; } = default!;
+    public OrderId OrderId { get; private set; } = default!;
+    public ProductId ProductId { get; private set; } = default!;
     public int Quantity { get; private set; } = default!;
     public decimal Price { get; private set; } = default!;
 }
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,6 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record OrderName
+{
+    public string Value { get; }
+}
