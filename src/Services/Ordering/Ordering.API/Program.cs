9fe5651c835cff3113190c097ceb3eb67fe202f1
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-04-30 19:15:16 -0500

    Include the DenpendencyInjection per project.

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,8 +1,19 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infraestructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 
+builder.Services
+    .AddApplicationServices()
+    .AddInfraestructureServices(builder.Configuration)
+    .AddApiServices();
+
 var app = builder.Build();
+
+app.UseApiServices();
 
 // Configure the HTTP request pipeline
 
---
+++ src/Services/Ordering/Ordering.Infraestructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infraestructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfraestructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        return services;
+    }
+}
