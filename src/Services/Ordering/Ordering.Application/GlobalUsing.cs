e533ea5277928a492aa154502491dd5a0452f2a8
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-02 17:53:17 -0500

    CreateOrder Command

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,1 +1,7 @@
-﻿
+﻿global using BuildingBlocks.CQRS;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Exceptions;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using FluentValidation;
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Order.CustomerId).NotEmpty().WithMessage("CustomerId is required");
+        RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not empty");
+    }
+}
