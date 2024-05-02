30527460c24de8d640754b26233589e7c36f1bf9
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-02 15:19:04 -0500

    Create the InitialSeed data and insert in Intilialaze method

--- src/Services/Ordering/Ordering.Infraestructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infraestructure/Data/Extensions/DatabaseExtensions.cs
@@ -12,5 +12,41 @@ public static class DatabaseExtensions
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        await SeedAsync(context);
+    }
+
+    public static async Task SeedAsync(ApplicationDbContext context)
+    {
+        await SeedCustomerAsync(context);
+        await SeedProductAsync(context);
+        await SeedOrderAndItemsAsync(context);
+    }
+
+    public static async Task SeedCustomerAsync(ApplicationDbContext context)
+    {
+        if (!await context.Customers.AnyAsync())
+        {
+            await context.Customers.AddRangeAsync(InitialData.Customers);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    public static async Task SeedProductAsync(ApplicationDbContext context)
+    {
+        if (!await context.Products.AnyAsync())
+        {
+            await context.Products.AddRangeAsync(InitialData.Products);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    public static async Task SeedOrderAndItemsAsync(ApplicationDbContext context)
+    {
+        if (!await context.Orders.AnyAsync())
+        {
+            await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+            await context.SaveChangesAsync();
+        }
     }
 }
--- src/Services/Ordering/Ordering.Infraestructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infraestructure/GlobalUsing.cs
@@ -1,2 +1,5 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using Ordering.Infraestructure.Data;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+
