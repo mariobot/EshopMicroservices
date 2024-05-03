c46a882bb972623a92c641ec28b8b2f28aa48b19
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-02 19:08:42 -0500

    GetOrdersQuery implementatio and add Pagination implementation.

---
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginationResult.cs
@@ -0,0 +1,11 @@
+﻿namespace BuildingBlocks.Pagination;
+
+public class PaginationResult<TEntity>
+    (int pageIndex, int pagSize, long count, IEnumerable<TEntity> data) 
+    where TEntity : class
+{
+    public int PageIndex { get; } = pageIndex;
+    public int PageSize { get; } = pagSize;
+    public long Count { get; } = count;
+    public IEnumerable<TEntity> Data { get; } = data;
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/Getorders/GetOrdersQuery.cs
@@ -0,0 +1,7 @@
+﻿using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public record GetOrdersQuery(PaginationRequest PaginationRequest) : IQuery<GetOrdersResult>;
+
+public record GetOrdersResult(PaginationResult<OrderDto> PaginationResult);
