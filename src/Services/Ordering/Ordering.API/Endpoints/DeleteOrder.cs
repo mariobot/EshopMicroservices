e9037f3f931d25be24837314169432864e596d17
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-06 16:43:24 -0500

    Create the Commands endpoints implementations.

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,5 +1,28 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
 
-public class DeleteOrder
+namespace Ordering.API.Endpoints;
+
+public record DeleteOrderRequest(Guid Id);
+
+public record DeleteOrderResponse(bool IsSuccess);
+
+public class DeleteOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{Id}", async (Guid Id, ISender sender) => 
+        {
+            var result = await sender.Send(new DeleteOrderCommand(Id));
+
+            var response = result.Adapt<DeleteOrderResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteOrder")
+        .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Order")
+        .WithDescription("Delete Order");
+    }
 }
