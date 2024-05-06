e4bb39f5c8f66bc44c41552359d0f91919530a45
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-06 18:53:53 -0500

    Add Healthy Checks to Ordering.API

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,15 +1,19 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
-using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCarter();
-
+        
         services.AddExceptionHandler<CustomExceptionHandler>();
+        
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
 
         return services;
     }
@@ -19,6 +23,8 @@ public static class DependencyInjection
         app.MapCarter();
 
         app.UseExceptionHandler(options => { });
+
+        app.UseHealthChecks("/health", new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse });
 
         return app;
     }
