b0e0b6bdfed48305bf068ba1b030a52c563e4563
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-07 18:23:44 -0500

    Creat the basic events classes

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,23 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+
+public record BasketCheckoutEvent : IntegrationEvent
+{
+    public Guid CustomerId { get; set; } = default!;
+    public decimal TotalPrice { get; set; } = default!;
+
+    // Shiping and BillingAddress
+    public string FirstName { get; } = default!;
+    public string LastName { get; } = default!;
+    public string EmailAddress { get; } = default!;
+    public string AddressLine { get; } = default!;
+    public string Country { get; } = default!;
+    public string State { get; } = default!;
+    public string ZipCode { get; } = default!;
+
+    // Payment
+    public string? CardName { get; } = default!;
+    public string CardNumber { get; } = default!;
+    public string Expiration { get; } = default!;
+    public string CVV { get; } = default!;
+    public int PaymentMethod { get; } = default!;
+}
