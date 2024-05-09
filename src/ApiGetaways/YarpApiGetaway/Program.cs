e624d39552fb710e44a2146db2b2922f3996c0d5
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-08 19:19:50 -0500

    Config yarp-reverseproxy in the YarpApiGetaway project

--- src/ApiGetaways/YarpApiGetaway/Program.cs
+++ src/ApiGetaways/YarpApiGetaway/Program.cs
@@ -2,8 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add service to the container
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-// Configure for the HTTP request pipeline. 
+// Configure for the HTTP request pipeline.
+app.MapReverseProxy();
 
 app.Run();
