8343f45a991174b1eb4b35b306446375f5197003
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-09 16:26:30 -0500

    Add RateLimiter for yarp configuration only to ordering-service

--- src/ApiGetaways/YarpApiGetaway/Program.cs
+++ src/ApiGetaways/YarpApiGetaway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service to the container
@@ -5,9 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
 var app = builder.Build();
 
 // Configure for the HTTP request pipeline.
+app.UseRateLimiter();
+
 app.MapReverseProxy();
 
 app.Run();
