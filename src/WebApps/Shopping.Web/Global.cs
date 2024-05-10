3b83810ecc4dd1b1ad8cf4268f11a8381b6ea53d
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-10 17:35:46 -0500

    Create IBasket and IOrdering Interfaces with Refit deff

--- src/WebApps/Shopping.Web/Global.cs
+++ src/WebApps/Shopping.Web/Global.cs
@@ -1,3 +1,5 @@
-﻿global using Refit;
-global using Shopping.Web.Services;
+﻿global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Models.Basket;
 global using Shopping.Web.Models.Catalog;
+global using Refit;
+global using Shopping.Web.Services;
---
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Web.Services;
+
+public interface IBasketService
+{
+    [Get("/basket-service/basket/{userName}")]
+    Task<GetBasketResponse> GetBasket(string userName);
+
+    [Post("/basket-service/basket/")]
+    Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
+
+    [Delete("/basket-service/basket/{userName}")]
+    Task<DeleteBasketResponse> DeleteBasket(string userName);
+
+    [Post("/basket-service/basket/checkout")]
+    Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+}
