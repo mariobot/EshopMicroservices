51750a98c2e9b03e386238ec92bf9b484f34edaa
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-05-13 19:32:01 -0500

    Create the Ordering Page

---
+++ src/WebApps/Shopping.Web/Pages/OrderList.cshtml.cs
@@ -0,0 +1,16 @@
+namespace Shopping.Web.Pages;
+
+public class OrderListModel(IOrderingService orderingService, ILogger<OrderListModel> logger) 
+    : PageModel
+{
+    public IEnumerable<OrderModel> Orders { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var customerId = new Guid("58c49479-ec65-4de2-86e7-033c546291aa");
+        var response = await orderingService.GetOrdersByCustomer(customerId);
+        Orders = response.Orders;
+
+        return Page();
+    }
+}
